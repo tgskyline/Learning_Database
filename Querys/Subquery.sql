147459a9a4443ef1f2d8ac388c8cc3e05ece00d7
tgskyline <tigomes.santos@gmail.com>
2022-01-21 06:17:21 -0300

    SUBQUERY

--- Querys/Subquery.sql
+++ Querys/Subquery.sql
@@ -1,4 +1,5 @@
 -- Subconsulta - SUBQUERY
+
 use bd_empresa
 
 /*
@@ -47,6 +48,65 @@ Outros operadores
 
 */
 
+-- Listar o nome dos empregados que possuem dependentes
 
+SELECT e.nom_empregado
+FROM empregado AS e
+WHERE e.num_matricula IN 
+(SELECT num_matricula FROM dependente)
 
+-- Listar o nome dos empregados que não possuem dependentes
 
+SELECT e.nom_empregado
+FROM empregado AS e
+WHERE e.num_matricula NOT IN 
+(SELECT num_matricula FROM dependente)
+
+-- Listar o nome dos empregados que possuem dependente
+
+SELECT e.nom_empregado
+FROM empregado as e 
+WHERE e.num_matricula IN (SELECT num_matricula FROM dependente)
+
+SELECT e.nom_empregado
+FROM empregado AS e WHERE EXISTS (SELECT * FROM dependente AS d WHERE d.num_matricula = e.num_matricula)
+
+SELECT DISTINCT e.nom_empregado FROM empregado as e
+JOIN dependente as d ON e.num_matricula = d.num_matricula
+
+-- Listar o nome dos empregados que não possuem dependente
+
+SELECT e.nom_empregado
+FROM empregado as e 
+WHERE e.num_matricula NOT IN (SELECT num_matricula FROM dependente)
+
+SELECT e.nom_empregado
+FROM empregado AS e WHERE NOT EXISTS (SELECT * FROM dependente AS d WHERE d.num_matricula = e.num_matricula)
+
+SELECT DISTINCT e.nom_empregado FROM empregado as e 
+LEFT JOIN dependente as d ON e.num_matricula = d.num_matricula
+WHERE d.nom_dependente IS NULL
+
+-- Subquerycomo tabela
+
+SELECT x.num_matricula, 
+x.nom_empregado
+FROM (SELECT * 
+FROM empregado) as x
+
+-- Listar o nome dos empregados que ganham o maior salário do seu respectivo departamento
+
+SELECT e.nom_empregado, 
+e.val_salario, 
+e.cod_depto 
+FROM empregado AS e JOIN (SELECT cod_depto, MAX(val_salario) AS maior 
+FROM empregado GROUP BY cod_depto) as M ON e.cod_depto = M.cod_depto WHERE e.val_salario = M.maior
+
+-- Listar o nome do empregado e o total de dependentes e cada um deles (exibir todos os empregado)
+
+SELECT e.nom_empregado, 
+(SELECT COUNT(*) 
+FROM  dependente d 
+WHERE d.num_matricula = e.num_matricula) 
+AS total
+FROM empregado e
